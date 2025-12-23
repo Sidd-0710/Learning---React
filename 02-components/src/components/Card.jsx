3c1c9c3d7e1398f8589c31254e044ad9286eb10b
Sidd_0710 <siddheshwarchirkute2003@gmail.com>
2025-12-23 17:52:59 +0530

    Components

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <>
+      <div className="card">
+        <h1>Sidd </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae quos hic, voluptatem fugiat in magnam. Ut, eveniet, voluptate, cupiditate cum maiores iure veritatis repellendus dolorum ipsam debitis quis cumque. Iste.</p>
+      </div>
+    </>
+  )
+}
+
+export default Card
