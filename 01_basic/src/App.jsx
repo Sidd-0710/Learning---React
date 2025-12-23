8fa6ec1d8e0705de37ce81598799a26d2c178293
Sidd_0710 <siddheshwarchirkute2003@gmail.com>
2025-12-23 17:24:58 +0530

    First File

---
+++ 01_basic/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id="dad">
+        <h1 id='child1'>Hello1</h1>
+        <h1 id='child2'>Hello2 </h1>
+      </div>
+      <div id='Chacha'></div>
+    </>
+  )
+}
+
+export default App
